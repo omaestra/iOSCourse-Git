dd16a384a2df139e523f8ee90750aef1da8e34b3
omaestra <om@visionarity.com>
2019-07-14 14:11:08 +0200

    Initial Commit

---
+++ iOSCourse-Git/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iOSCourse-Git
+//
+//  Created by omaestra on 14/07/2019.
+//  Copyright © 2019 omaestra. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
